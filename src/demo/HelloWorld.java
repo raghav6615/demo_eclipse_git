77c7a90e33f99ba6eebf24bab69a128d2dd9f586
raghav6615 <mohataraghav12@gmail.com>
2021-06-07 15:45:06 +0530

    FIRST COMMIT

---
+++ src/demo/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package demo;
+
+/**
+ * @author ADMIN
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO");
+	}
+
+}
